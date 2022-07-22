ae1bf4a5899e2fa03784646917758a4b45aa60e6
Bernd Doser <bernd.doser@braintwister.eu>
2022-07-22 07:41:58 +0000

    generic square

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,28 @@
-// Type your code here, or load an example.
-pub fn square(num: i32) -> i32 {
+use std::{ops::Mul, marker::Copy};
+
+pub fn square_i32(num: i32) -> i32 {
     num * num
 }
 
-// If you use `main()`, declare it as `pub` to see it in the output:
-pub fn main()
+pub fn square_f32(num: f32) -> f32 {
+    num * num
+}
+
+pub fn square<T>(num: T) -> T where T: Mul<Output = T> + Copy {
+    num * num
+}
+
+pub(crate) fn main()
 {
     println!("hey");
-    let a = square(10);
+    let a = square_i32(10);
     println!("{}", a);
+
+    let b: f32 = 2.5;
+    let c = square_f32(b);
+    println!("{}", c);
+
+    let d = 1.5;
+    let e = square(d);
+    println!("{}", e);
 }
