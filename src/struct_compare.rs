54f8d09b4e14e0609c6b24eaab1a82fe2058cfdc
Bernd Doser <bernd.doser@h-its.org>
2022-07-27 11:26:20 +0000

    impl PartialEq::eq for point struct

---
+++ src/struct_compare.rs
@@ -0,0 +1,20 @@
+struct Point<T>
+{
+    pub x: T,
+    pub y: T,
+}
+
+impl<T: PartialEq> PartialEq for Point<T> {
+    fn eq(&self, other: &Self) -> bool {
+        self.x == other.x && self.y == other.y
+    }
+}
+
+fn main()
+{
+    let p1 = Point{x: 1.2, y:2.3};
+    let p2 = Point{x: 1.2, y:2.3};
+
+    assert_eq!(p1 == p2, true);
+    assert_eq!(p1 != p2, false);
+}
